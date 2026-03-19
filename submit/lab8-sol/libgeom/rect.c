b8aca49a3c3d2a6f16da0210bb6aaf04e423ac00
rfeng2 <rfeng2@cs.binghamton.edu>
2026-03-19 15:50:42 -0400

    Completed lab8

---
+++ submit/lab8-sol/libgeom/rect.c
@@ -0,0 +1,23 @@
+#include "geom.h"
+
+/** Return perimeter of rectangle with lower-left corner at p0 and
+ *  upper-right corner at p1.
+ */
+double
+rect_perimeter(Point p0, Point p1)
+{
+  int width = p1.x - p0.x;
+  int height = p1.y - p0.y;
+  return 2 * (width + height);
+}
+
+/** Return area of rectangle with lower-left corner at p0 and
+ *  upper-right corner at p1.
+ */
+double
+rect_area(Point p0, Point p1)
+{
+  int width = p1.x - p0.x;
+  int height = p1.y - p0.y;
+  return (width * height);
+}
---
+++ submit/lab8-sol/libvec/.gdbinit
@@ -0,0 +1,2 @@
+set logging enabled on
+set trace-commands on
